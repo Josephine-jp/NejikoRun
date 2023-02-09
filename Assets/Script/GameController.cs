e8d3e2e4881f02a003669ce73dfa33404e3e4764
josephine <nuts5569@yahoo.co.jp>
2023-02-09 21:37:19 +0900

    UI追加

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public NejikoController nejiko;
+    public Text scoreText;
+    public LifePanel lifePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        int score = CalcScore();
+        scoreText.text = "Score : " + score + "m";
+
+        lifePanel.UpdateLife(nejiko.Life());
+    }
+
+    int CalcScore()
+    {
+        return (int) nejiko.transform.position.z;
+    }
+}
